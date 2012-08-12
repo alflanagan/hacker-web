e1b034b2915cfb42ad80fde4844a1938e8ae2f76
jduncan-rva <jamie.e.duncan@gmail.com>
2012-08-12 16:08:49 -0400

    cleaned up urls.py

--- urls.py
+++ urls.py
@@ -14,11 +14,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^goalfish/', include('goals.urls')),
-	url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^profiles/', include('profiles.urls')),
-	url(r'^$', include('goals.urls')),
-    url(r'^goals/', include('goals.urls')),
+    url(r'^$', include('goals.urls')),
     url(r'^messages/', include('django_messages.urls')),
     url(r'^notifications/', include('notification.urls')),
 )
