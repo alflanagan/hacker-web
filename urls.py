01f55c70cbfe105b39eedff0d67704a550f9e61f
jduncan-rva <jamie.e.duncan@gmail.com>
2012-09-17 00:03:32 -0400

    getting the basic templates working

--- urls.py
+++ urls.py
@@ -5,12 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/$', include('members.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^profiles/', include('profiles.urls')),
     url(r'^messages/', include('django_messages.urls')),
