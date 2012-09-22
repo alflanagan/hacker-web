ab4f9ca3ac1a60d9cac9d8e8af53878c53a93349
jduncan <jamie.e.duncan@gmail.com>
2012-09-22 00:21:15 -0400

    starting on workshops app in earnest

--- workshops/models.py
+++ workshops/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-from sponsors.models import Sponsor
+
+class WorkshopCategory(models.Model):
+
+    name = models.CharField(max_length=32, unique=True, help_text="name for this category")
+    description = models.TextField(blank=True, null=True, help_text="optional notes")
 
 class Workshop(models.Model):
     '''
     'defines workshops that the space can hold
     '''
-    name = models.CharField(max_length=32, unique=True, help_text="name for the class")
-    instructor = models.ForeignKey(User, related_name='instructor_user', help_text="person presenting the workshop")
-    attendees = models.ManyToManyField(User, related_name='attendee_user', help_text="people taking the workshop")
+    name = models.CharField(max_length=32, unique=True, help_text="name for the workshop")
+    category = models.ForeignKey(ModelCategory, help_text="category this workshop falls under")
+    number = models.CharField(max_length=4, help_text="number for class. e.g. 101, 500, etc.")
+    instructor = models.ForeignKey(User, help_text="person presenting the workshop")
     date = models.DateField(help_text="date workshop will occur")
     start_time = models.TimeField(help_text="workshop start time")
     end_time = models.TimeField(help_text="workshop end time")
     date_created = models.DateField(auto_now_add=True)
-    sponsor = models.ForeignKey(Sponsor, null=True, blank=True)
 
     def __unicode__(self):
-        return self.name
+        return "%s %s-%s" % (self.category.name, self.number, self.name)
 
-    @property
-    def is_sponsored(self):
-        if self.sponsor:
-            return True
-        else: 
-            return False
+
