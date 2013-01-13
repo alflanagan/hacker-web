78101a050183111c3dd051011d3d849816be8208
jduncan-rva <jamie.e.duncan@gmail.com>
2013-01-13 17:30:30 -0500

    removing the MemberLevel model. It will be definable only by the billable info that we can pull in from intuit. all members are equal

--- members/models.py
+++ members/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from projects.models import UserProjects
 from django.db.models.signals import post_save, post_syncdb
+from spaces.models import HackerSpace
 
 """
 "
@@ -20,31 +21,32 @@ class MemberInterests(models.Model):
     def __unicode__(self):
         return self.name
 
-class MemberLevel(models.Model):
-    '''
-    used to define member levels for various users
-    '''
-    name = models.CharField(max_length=64, unique=True, help_text="Membership Level Name")
-    fee = models.PositiveIntegerField(help_text="fee for this level of membership")
-    notes = models.TextField(null=True, blank=True, help_text="Optional Notes")
-
-    def __unicode__(self):
-        return self.name
+    class Meta:
+        verbose_name_plural = 'Member Interests'
 
 class MemberProfile(models.Model):
     '''
     extending the default User model
     '''
+
     user = models.OneToOneField(User)
-    is_active = models.BooleanField(default=True, help_text="whether or not you are active within the space currently")
-    level = models.ForeignKey(MemberLevel, unique_for_month="membership_change_date")
+    hackerspace = models.ForeignKey(HackerSpace, blank=True, null=True, help_text="the space the member belongs to")
     membership_change_date = models.DateField(auto_now=True, help_text="you can chanage your membership type once each calendar month")
-    projects = models.ManyToManyField(UserProjects)
-    interests = models.ManyToManyField(MemberInterests)
+    projects = models.ManyToManyField(UserProjects, blank=True, null=True)
+    interests = models.ManyToManyField(MemberInterests, blank=True, null=True)
     rsa_key = models.TextField(null=True, blank=True, help_text="optional RSA Public Key")
     pgp_key = models.TextField(null=True, blank=True, help_text="optional PGP Key")
     avatar = models.ImageField(null=True, blank=True, upload_to="avatars/", help_text="optional avatar (jpg,gif,png)")
 
     def __unicode__(self):
         return str(self.user)
+
+    class Meta:
+        verbose_name = 'Member Profile'
+
+def create_member_profile(sender, instance, created, **kwargs):
+    if created:
+        MemberProfile.objects.create(user=instance)
+
+post_save.connect(create_member_profile, sender=User)
 
