a9493d7d1a04a0a68307cd6c774482078c8c9c0f
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2017-02-24 17:45:56 -0500

    Fix keyword param that changed name

--- members/views.py
+++ members/views.py
@@ -1,17 +1,18 @@
+"""Views related to / used by members (not admin)."""
 from django.template import RequestContext
-from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
 from projects.models import UserProjects
 
 def index(request):
-
+    """Basic member view, showing avatar and projects."""
     if request.user.is_authenticated():
         avatar_url = settings.DEFAULT_AVATAR_URL
         project_list = UserProjects.objects.filter(user=request.user).order_by('start_date')
 
-        return render_to_response('members/index.html',context_instance = RequestContext(request,{"default_avatar_url":avatar_url,"project_list":project_list}))
+        return render_to_response('members/index.html',
+                                  context=RequestContext(request,
+                                                         {"default_avatar_url": avatar_url,
+                                                          "project_list": project_list}))
     else:
         return render_to_response('members/index.html', {})
--- workshops/views.py
+++ workshops/views.py
@@ -1,19 +1,18 @@
 from django.template import RequestContext
-from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
-from django.conf import settings
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from projects.models import UserProjects, UserProjectsForm
 
 @login_required
 def project_detail(request, proj_id):
 
-    project = get_object_or_404(UserProjects,pk=proj_id)
+    project = get_object_or_404(UserProjects, pk=proj_id)
 
     #so only staff users and the users themselves can see a given project page.
-    if ((request.user.username == project.user.username) or (request.user.is_staff)):
-        return render_to_response('projects/detail.html',context_instance = RequestContext(request,{"project":project,}))
+    if (request.user.username == project.user.username) or (request.user.is_staff):
+        return render_to_response('projects/detail.html',
+                                  context=RequestContext(request, {"project":project,}))
     else:
         redir_url = "/members/profiles/%s" % project.user.username
         return HttpResponseRedirect(redir_url)
@@ -21,13 +20,13 @@ def project_detail(request, proj_id):
 @login_required
 def project_delete(request, proj_id):
 
-    project = get_object_or_404(UserProjects,pk=proj_id)
+    project = get_object_or_404(UserProjects, pk=proj_id)
 
-    if ((request.user.username == project.user.username) or (request.user.is_superuser)):
+    if (request.user.username == project.user.username) or (request.user.is_superuser):
         project.delete()
-        return render_to_response('projects/delete.html',context_instance = RequestContext(request,{}))
+        return render_to_response('projects/delete.html', context=RequestContext(request,{}))
     else:
-        return render_to_response('projects/no_delete.html',context_instance = RequestContext(request,{}))
+        return render_to_response('projects/no_delete.html', context=RequestContext(request,{}))
 
 @login_required
 def project_create(request):
@@ -39,7 +38,8 @@ def project_create(request):
     else:
         form = UserProjectsForm(initial={'user':request.user})
 
-    return render_to_response('projects/edit.html',context_instance = RequestContext(request,{'form':form}))
+    return render_to_response('projects/edit.html',
+                              context = RequestContext(request,{'form':form}))
 
 @login_required
 def project_edit(request, proj_id):
@@ -49,9 +49,11 @@ def project_edit(request, proj_id):
         form.save()
         return HttpResponseRedirect('/projects/saved/')
 
-    return render_to_response('projects/edit.html',context_instance = RequestContext(request,{'form':form}))
+    return render_to_response('projects/edit.html',
+                              context=RequestContext(request,{'form':form}))
 
 @login_required
 def project_saved(request):
 
-    return render_to_response('projects/saved.html',context_instance = RequestContext(request,{}))
+    return render_to_response('projects/saved.html',
+                              context=RequestContext(request,{}))
